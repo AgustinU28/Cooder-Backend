c90c8217062a0750cd4cb573518daf6adc4abe2e
agustinuranga@live.com <agustinuranga@live.com>
2023-02-19 16:11:36 -0300

    primer entrega

---
+++ entrega/index.js
@@ -0,0 +1,41 @@
+let nombre = 'Ana'
+nombre = 'Camila'
+
+const objeto = {
+    nombre: 'Agustin',
+    apellido: 'Uranga'
+}
+
+
+
+let array = []
+array = [true]
+
+console.log(typeof (objeto));
+console.log(objeto);
+
+
+
+let obj1 = {
+    name: "Nico",
+    edad: 25
+}
+console.log(obj1);
+
+
+const obj2 = { ...obj1 }
+console.log(obj2);
+
+obj2.name = "Lucas"
+obj2.pais = "Arg"
+
+console.log(obj2);
+
+console.log('Objeto 1');
+console.log(obj1);
+
+
+const array2 = [1, 2, 3, 4, "perro"]
+
+
+array2.push(34)
